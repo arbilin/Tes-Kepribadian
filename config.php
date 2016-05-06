3e87893ba26cd401adac6cb62c18bfe1ed5150cc
arbilin <brilianrahmat@gmail.com>
2016-05-06 10:01:22 +0800

    Added files via upload

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 //File PHP berikut berfungsi untuk mengkoneksikan halaman web dengan database mysql
-  $host="localhost"; //Nama host. Secara default kita tulis saja "localhost" (karena pakai XAMPP)
-  $user="root"; //Username. Secara default kita tulis saja "root" (karena pakai XAMPP)
-  $password=""; //Password. Secara default kita tulis saja dengan nilai kosong
+  $host="br-cdbr-azure-south-b.cloudapp.net"; 
+  $user="b330ec0532bdcb"; 
+  $password="0da9feb1"; 
   $database="db_tk"; //Nama database yang akan kita gunakan
   mysql_connect($host,$user,$password);
   mysql_select_db($database);
